f7ecb0098cc7d64bdb1621a21209e80d770dc2d5
Alexander <alex20k.x@gmail.com>
2019-08-24 21:18:55 +0600

    Big words, Best scores

--- viewsFile.py
+++ viewsFile.py
@@ -19,42 +19,52 @@ def index():
     if current_user.is_authenticated:
         if request.method == 'GET':
             scores = current_user.scores
-            return render_template('game.html', palnforms=palnforms, scores=scores)
+
+            users = User.query.all()
+
+            users_scores = [user.scores for user in users]
+
+            best_scores = sorted(users_scores, reverse=True)[:3]
+            best_users = [user for user in users if user.scores in best_scores]
+
+            return render_template('game.html', palnforms=palnforms, scores=scores, best_users=best_users)
 
         if request.method == 'POST':
             palindrome = request.form['palindromeform'].lower()
-            if palindrome in rus_words:  # Существует ли это слово
-                if palindrome == palindrome[::-1]:  # Палиндром ли это
-                    p = Palindrome.query.filter(Palindrome.title == palindrome).first()
+            for words in palindrome.split():  # Существует ли это слово
+                if words in rus_words:
+                    palindrome_letters = list(palindrome.replace(' ', ''))
+                    if palindrome_letters == palindrome_letters[::-1]:  # Палиндром ли это
+                        p = Palindrome.query.filter(Palindrome.title == palindrome).first()
 
-                    try:
-                        palindrome_users = p.users
-                    except AttributeError:
-                        new_palindrome = Palindrome(title=palindrome)
-                        db.session.add(new_palindrome)
-                        db.session.commit()
+                        try:
+                            palindrome_users = p.users
+                        except AttributeError:
+                            new_palindrome = Palindrome(title=palindrome)
+                            db.session.add(new_palindrome)
+                            db.session.commit()
 
-                        current_user.scores += len(palindrome)
-                        new_palindrome.users.append(current_user)
-                        db.session.commit()
-
-                        flash('Успешно! Вам начислено {} очко(в)'.format(len(palindrome)))
-                        return redirect('/')
-                    else:
-                        if current_user not in palindrome_users.all(): # Не повторяет ли пользователь этот палиндром
                             current_user.scores += len(palindrome)
-                            palindrome_users.append(current_user)
+                            new_palindrome.users.append(current_user)
                             db.session.commit()
 
                             flash('Успешно! Вам начислено {} очко(в)'.format(len(palindrome)))
                             return redirect('/')
-
                         else:
-                            flash('Вы уже использовали это слово!')
-                            return redirect('/')
-                else:
-                    flash('Введёное вами слово - не палиндром!')
-                    return redirect('/')
+                            if current_user not in palindrome_users.all(): # Не повторяет ли пользователь этот палиндром
+                                current_user.scores += len(palindrome)
+                                palindrome_users.append(current_user)
+                                db.session.commit()
+
+                                flash('Успешно! Вам начислено {} очко(в)'.format(len(palindrome)))
+                                return redirect('/')
+
+                            else:
+                                flash('Вы уже использовали это слово!')
+                                return redirect('/')
+                    else:
+                        flash('Введёное вами слово - не палиндром!')
+                        return redirect('/')
             else:
                 flash('Введённого слова не существует!')
                 return redirect('/')
